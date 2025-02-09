cd5d2144cfa1b3b26b9974d7f37359d4000c601e
semajame <jamesanquillano@gmail.com>
2025-02-09 23:39:33 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import ResumeForm from '../app/components/ResumeForm'
 import ModeToggle from '../app/components/mode-toggle'
 import Footer from './components/Footer'
-import Header from './components/Header'
-import LandingPage from './components/LandingPage'
 
 export default function Home() {
   return (
