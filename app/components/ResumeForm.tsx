20a19fe5fdc44f9e08bbe8002301798067f8ad41
James Sevilla Anquillano <142220813+semajame@users.noreply.github.com>
2025-02-12 21:58:42 +0800

    Update ResumeForm.tsx

--- app/components/ResumeForm.tsx
+++ app/components/ResumeForm.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import { Ellipsis } from 'lucide-react'
-import { supabase } from '@/lib/supabase'
+
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
